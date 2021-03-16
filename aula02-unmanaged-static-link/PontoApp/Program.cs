8adfb624d8a5824cbda03a1ac24a291e8181ddd4
bigskydiver <pedrocarneirobatista@gmail.com>
2021-03-16 21:25:30 +0000

    fix

--- aula02-unmanaged-static-link/PontoApp/Program.cs
+++ aula02-unmanaged-static-link/PontoApp/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-
+using PontoLib;
 namespace PontoApp
 {
     class Program
     {
+		public static void demo1(){
+			Point p = new Point (3,7);
+			Console.WriteLine("Module = " + p.getModule());
+		}
         static void Main(string[] args)
         {
+			demo1();
             Console.WriteLine("Hello World!");
         }
     }
